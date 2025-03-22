67221c821c2091307dcefcd5719c50b224711d23
fait-arch <jdmoromenacho@gmail.com>
2025-03-22 03:23:53 -0500

    Create opcion DarkTheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
+'use client'
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+// Componente ModeToggle
+export function ModeToggle() {
+	const { setTheme } = useTheme()
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" size="icon">
+					<Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+					<Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+					<span className="sr-only">Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem onClick={() => setTheme('light')}>
+					Light
+				</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('dark')}>
+					Dark
+				</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('system')}>
+					System
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
+// Componente de página principal que debe ser exportado como default
 export default function Home() {
-	return <h1 className="text-3xl font-bold underline"> 
-    Hello world! 
-  </h1>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<ModeToggle />
+		</main>
+	)
 }
